bed80c2cc3b2f401d24af4b1500557c10cf9108a
Thejas Kiran P S <thejaskiranps@gmail.com>
2023-02-16 19:59:18 +0530

    add error handling for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
     println!("Searching for `{0}` in {1}", config.query, config.file_path);
 
     let file_contents = fs::read_to_string(config.file_path).expect("Cannot read file.");
@@ -15,10 +19,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments.");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Self { query, file_path }
+        Ok(Self { query, file_path })
     }
 }
