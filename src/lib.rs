bd05e7d997c99fce55691dc0237a3fc2b0c620d6
Thejas Kiran P S <thejaskiranps@gmail.com>
2023-02-16 20:20:43 +0530

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
 pub struct Config {
     pub query: String,
     pub file_path: String,
@@ -21,5 +25,21 @@ impl Config {
         let file_path = args[2].clone();
 
         Ok(Self { query, file_path })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
