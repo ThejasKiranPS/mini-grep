807d3599790250ff6a47415ef2337d020611aa4c
Thejas Kiran P S <thejaskiranps@gmail.com>
2023-02-16 20:09:56 +0530

    refactor: create lib.rs to hold api

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+use minigrep::{run, Config};
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -14,28 +13,5 @@ fn main() {
     if let Err(e) = run(config) {
         print!("Application Error: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let file_contents = fs::read_to_string(config.file_path)?;
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments.");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Self { query, file_path })
     }
 }
