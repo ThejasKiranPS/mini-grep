e5b102f02afb99fb343be926cedbcbce9676df17
Thejas Kiran P S <thejaskiranps@gmail.com>
2023-02-16 19:34:25 +0530

    collect arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for `{0}` in {1}", query, file_path);
 }
