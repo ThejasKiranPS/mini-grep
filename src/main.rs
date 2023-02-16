4bba11f2fc2217c19355f7fe277d3f7101692cb4
Thejas Kiran P S <thejaskiranps@gmail.com>
2023-02-16 19:51:40 +0530

    refactor: store parsed args in a struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
+    println!("Searching for `{0}` in {1}", config.query, config.file_path);
 
-    println!("Searching for `{0}` in {1}", query, file_path);
+    let file_contents = fs::read_to_string(config.file_path).expect("Cannot read file.");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Self {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Self { query, file_path }
+    }
 }
