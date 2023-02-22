f7312cd91bfc3437af15374e76b99058602eefb5
Thejas Kiran P S <thejaskiranps@gmail.com>
2023-02-22 11:42:38 +0530

    make use of iterators in searching & args-parsing functions

--- src/lib.rs
+++ src/lib.rs
@@ -18,25 +18,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut res: Vec<&str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            res.push(line)
-        }
-    }
-
-    res
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut res: Vec<&str> = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            res.push(line)
-        }
-    }
-    res
+    contents
+        .lines()
+        .filter(|line| (line.to_lowercase()).contains(&query.to_lowercase()))
+        .collect()
 }
 
 pub struct Config {
@@ -46,12 +38,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments.");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Self, &'static str> {
+        // First argument will be the filename itself. Ignoring it.
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get any file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Self {
